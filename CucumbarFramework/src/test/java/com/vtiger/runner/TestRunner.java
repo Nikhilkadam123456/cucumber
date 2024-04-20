b682930658d6da82c2a9e1430981fd57ca8c2ee0
ADMIN <ADMIN@DESKTOP-561M5F8>
2024-04-20 12:54:21 +0530

    test branch

--- CucumbarFramework/src/test/java/com/vtiger/runner/TestRunner.java
+++ CucumbarFramework/src/test/java/com/vtiger/runner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		
 		features = "src/test/resources/Features",
 		glue="com.vtiger.stepdefination",
-		dryRun = false,
+		dryRun = true,
 		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
 				             "junit:target/cucumber-reports/Cucumber.xml",
 				             "html:target/cucumber-reports.html"}
